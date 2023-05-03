a28439fcc3f5030455f2604ed66fc55df28dfcae
Anthony Elison <tonyelison37@gmail.com>
2023-05-03 17:43:48 -0600

    Add webpack config for images, import icon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './bell.svg';
 
 function component() {
   const element = document.createElement('div');
@@ -7,6 +8,13 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  myIcon.classList.add('icon');
+  
+  element.appendChild(myIcon);
 
   return element;
 }
