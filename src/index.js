3fe6d434601b8790faa4e3a0304dc5cf2c055610
Anthony Elison <tonyelison37@gmail.com>
2023-05-03 18:27:31 -0600

    Remove assets, loaders, prep for output tutorial

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './bell.svg';
-import Data from './example-spreadsheet.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  myIcon.classList.add('icon');
-
-  element.appendChild(myIcon);
-
-  // log csv data
-  console.log(Data);
 
   return element;
 }
