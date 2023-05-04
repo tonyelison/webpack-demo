94419c4023ba2a38f3d3e257011a4a24254f445a
Anthony Elison <tonyelison37@gmail.com>
2023-05-03 18:05:10 -0600

    Add example csv data and import into script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './bell.svg';
+import Data from './example-spreadsheet.csv';
 
 function component() {
   const element = document.createElement('div');
@@ -13,8 +14,11 @@ function component() {
   const myIcon = new Image();
   myIcon.src = Icon;
   myIcon.classList.add('icon');
-  
+
   element.appendChild(myIcon);
+
+  // log csv data
+  console.log(Data);
 
   return element;
 }
