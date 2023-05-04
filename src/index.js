a80bde1be7d3a395e8ca5746e1630c4f50cc8587
Anthony Elison <tonyelison37@gmail.com>
2023-05-03 18:14:54 -0600

    Add json, toml, & yaml loaders, config, and data files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './bell.svg';
 import Data from './example-spreadsheet.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement('div');
